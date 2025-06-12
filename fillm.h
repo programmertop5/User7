a9a5a17390a6ed998cb43fe8acddf80b51d35a2e
programmertop5 <kardrkarina@gmail.com>
2025-06-12 15:14:58 +0300

    Update film.h

--- fillm.h
+++ fillm.h
@@ -11,19 +11,18 @@ struct Film {
     int quality = 0;
 };
 
-// Создает новый фильм с заданными параметрами
+
 Film createNewFilm(const char* name, const char* author, const char* genre, const char* description, const char* date, int quality);
 
-// Создает новый фильм, запрашивая данные у пользователя
+
 Film createNewFilmFromUserInput();
 
-// Выводит информацию о фильме
+
 void printFilm(const Film& film);
 
-// Выводит информацию о всех фильмах из массива
+
 void printFilms(Film* films, int size);
 
-// Освобождает память, занятую полями фильма
 void deleteFilm(Film& film);
 
 // Освобождает память всех фильмов в массиве
